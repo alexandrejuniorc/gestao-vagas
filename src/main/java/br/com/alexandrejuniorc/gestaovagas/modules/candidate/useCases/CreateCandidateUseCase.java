5e9882aa1ea25e6eb749f3f457b3814185cb635d
Alexandre <alejunior.dev@gmail.com>
2023-12-15 17:46:43 -0300

    feat: encrypt candidate password

--- src/main/java/br/com/alexandrejuniorc/gestaovagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/alexandrejuniorc/gestaovagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package br.com.alexandrejuniorc.gestaovagas.modules.candidate.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.alexandrejuniorc.gestaovagas.exceptions.ExceptionUserFound;
@@ -12,11 +13,17 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new ExceptionUserFound();
                 });
+
+        var password = this.passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
 
         return this.candidateRepository.save(candidateEntity);
     }
