fb42ba0cc2a1a57f4bee797d5356067ffa975efb
Alexandre <alejunior.dev@gmail.com>
2023-12-19 17:30:02 -0300

    feat: configure the swagger

--- src/main/java/br/com/alexandrejuniorc/gestaovagas/GestaoVagasApplication.java
+++ src/main/java/br/com/alexandrejuniorc/gestaovagas/GestaoVagasApplication.java
@@ -3,7 +3,11 @@ package br.com.alexandrejuniorc.gestaovagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Gestão de vagas", version = "1.0.0", description = "API para gestão de vagas"))
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
