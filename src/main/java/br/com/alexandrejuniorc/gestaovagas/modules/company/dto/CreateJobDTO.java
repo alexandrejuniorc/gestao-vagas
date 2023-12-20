0cd7daf6a959c0dbee11646b77e701d525074373
Alexandre <alejunior.dev@gmail.com>
2023-12-20 12:01:59 -0300

    feat(swagger): insert examples on requests

--- src/main/java/br/com/alexandrejuniorc/gestaovagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/alexandrejuniorc/gestaovagas/modules/company/dto/CreateJobDTO.java
@@ -1,12 +1,20 @@
 package br.com.alexandrejuniorc.gestaovagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class CreateJobDTO {
+
+    @Schema(example = "Job for Java developer", requiredMode = RequiredMode.REQUIRED)
     private String description;
+
+    @Schema(example = "Gympass, VR/VA", requiredMode = RequiredMode.REQUIRED)
     private String benefits;
+
+    @Schema(example = "Senior", requiredMode = RequiredMode.REQUIRED)
     private String level;
 }
--- src/main/java/br/com/alexandrejuniorc/gestaovagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/alexandrejuniorc/gestaovagas/modules/company/entities/JobEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -28,17 +29,22 @@ public class JobEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Schema(example = "Job for Java developer")
     private String description;
+
+    @Schema(example = "Gympass, VR/VA")
     private String benefits;
 
     @NotBlank(message = "O campo [level] não pode ser vazio!")
+    @Schema(example = "Senior")
     private String level;
 
     @ManyToOne
     @JoinColumn(name = "company_id", insertable = false, updatable = false)
     private CompanyEntity companyEntity;
 
-    @Column(name = "company_id", nullable = false)
+    @Column(name = "company_id")
     private UUID companyId;
 
     @CreationTimestamp
